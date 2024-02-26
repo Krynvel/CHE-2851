4d27199d9eb855828b42b00a1eda818f5d239fb7
goldpandasnow <159591191+goldpandasnow@users.noreply.github.com>
2024-02-26 11:42:07 -0800

    Delete stg1_Q.m

--- stg1_Q.m
+++
@@ -1,5 +0,0 @@
-'initial general function structure; variables need revising'
-
-function dQdx = stg1_Q(Q_f,Q_p,x,A,L_p,deltaP_r1,deltapi_0);
--dQdx = A * L_p * (deltaP_r1-(Q_f/Q_p)*deltapi_0);
-end
