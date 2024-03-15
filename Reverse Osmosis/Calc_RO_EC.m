09f1c4d29def0f09f4dd2f707b9fd92f4b9a442d
goldpandasnow <159591191+goldpandasnow@users.noreply.github.com>
2024-03-14 19:27:01 -0700

    Update Calc_RO_EC.m

--- Reverse Osmosis/Calc_RO_EC.m
+++ Reverse Osmosis/Calc_RO_EC.m
@@ -6,16 +6,15 @@ H = zeros(1,10); % Pump Head (ft.)
 rcv = zeros(1,10); % Recovery Percentage
 pi_0 = 9; % Feed Osmotic Pressure (psi)
 
-
+options = optimset('TolFun',1e-10,'TolX',1e-10,'Display','off');
 for i = 1:10
    rcv(i) = 0.76+0.02*i; % Percent Recovery
    Q_0(i) = 1234/rcv(i); % Feed Flow (gpm)
-   H(i) = fsolve(@(H)Calc_H(H,Q_0(i),rcv(i)),180); % Pump Head (ft.)
+   H(i) = fsolve(@(H)Calc_H(H,Q_0(i),rcv(i)),360, options); % Pump Head (ft.)
    Y = Calc_RO_Y(Q_0(i),H(i)); % Energy Consumption
 end
 H
 EC = Q_0.*H./(1234.*pi_0); % Energy Consumption
-
 
 plot(rcv,EC,"LineStyle","-","Color","b","Marker","o","MarkerEdgeColor","r")
 xlabel('Pump Recovery')
