475c62f56fd75484e0b5ea3a1c4e1a520d65dde0
goldpandasnow <159591191+goldpandasnow@users.noreply.github.com>
2024-03-14 21:55:30 -0700

    Update Calc_RO_Y.m
    Updated % Labels

--- Reverse Osmosis/Calc_RO_Y.m
+++ Reverse Osmosis/Calc_RO_Y.m
@@ -1,22 +1,23 @@
-%% Part B, 1.)
-function Y = Calc_RO_Y(Q_0,H)
+%% Part B, 3.)
 
-H = 360; % Pump Head (ft.)
-Q_0 = 1525; % Feed Flow (gpm)
-%% Stage 1
-Am = 28*7*400; % Membrane Area (ft^2)
-Lp = 0.11/24/60; % (gallon per ft^2 per min/per psi)
-k = 2.12e-5; % (psi/gpm^2)
-pi_0 = 9;  % Feed Osmotic Pressure(psi)
- 
-dP_0 = 40.6+H*0.4327-16.4; % Transmembrane Inlet Pressure (psi)
-[x,QP_1]=ode45(@(x,QP_1)RO_model_fxn(x,QP_1,Am,Lp,k,Q_0,pi_0),[0 1],[Q_0,dP_0]);
-%% Stage 2
-Q_1 = QP_1(end,1); % Calls final value from Stage 1
-dP_1 = QP_1(end,2); % Calls final value from Stage 1
-Am_2 = Am/2; % Membrane Area (ft^2)
-Lp_2 = Lp; % (gallon per ft^2 per min/per psi)
-k2 = k*4; % (psi/gpm^2)
-[x2,QP_2]=ode45(@(x,QP_1)RO_model_fxn(x,QP_1,Am_2,Lp_2,k2,Q_0,pi_0),[1 2],[Q_1,dP_1]);
+function Y = Calc_RO_EC()
+Y = zeros(1,10); % Energy Consumption
+Q_0 = zeros(1,10); % Feed Flow (gpm)
+H = zeros(1,10); % Pump Head (ft.)
+rcv = zeros(1,10); % Recovery Percentage
+pi_0 = 9; % Feed Osmotic Pressure (psi)
 
-Y = 1 - QP_2(end,1)/Q_0;
+options = optimset('TolFun',1e-10,'TolX',1e-10,'Display','off');
+for i = 1:10
+   rcv(i) = 0.76+0.02*i; % Percent Recovery
+   Q_0(i) = 1234/rcv(i); % Feed Flow Rate (gpm)
+   H(i) = fsolve(@(H)Calc_H(H,Q_0(i),rcv(i)),360, options); % Pump Head (ft.)
+   Y = Calc_RO_Y(Q_0(i),H(i)); % Energy Consumption
+end
+EC = Q_0.*H./(1234.*pi_0); % Energy Consumption
+
+plot(rcv,EC,"LineStyle","-","Color","b","Marker","o","MarkerEdgeColor","r")
+xlabel('Pump Recovery')
+ylabel('Energy Consumption');
+title ("Energy Consumption vs. Recovery ")
+end 
