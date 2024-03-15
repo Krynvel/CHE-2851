a46daa5d18c4d075f05a67cadf08e56d201720f7
goldpandasnow <159591191+goldpandasnow@users.noreply.github.com>
2024-03-14 22:17:47 -0700

    Update RO_model_run.m
    % label updates

--- Reverse Osmosis/RO_model_run.m
+++ Reverse Osmosis/RO_model_run.m
@@ -1,14 +1,15 @@
 %% Part A, 2.)
 
 %% Stage 1
-
+function Y = RO_model_run (Q_0,H)
 % Given Parameters
 Am = 28*7*400; % Membrane Area (ft^2)
 Lp = 0.11/24/60; % Membrane Hydraulic Permeability (gpm/psi)
 k = 2.12e-5; % Constant for Stage 1 (psi/gpm^2)
 pi_0 = 9; % Osmotic Pressure (psi)
 Q_0 = 1525; % Feed Flow Rate (gpm)
-dP_0 = 40.6+360*0.433-16.4; % Transmembrane Osmotic Inlet Pressure (psi)
+H = 360; % Pump Head (ft.)
+dP_0 = 40.6+(H*0.4327)-16.4; % Transmembrane Osmotic Inlet Pressure (psi)
 
 % Initialize Diff. EQ for Stage 1
 [x,QP_1]=ode45(@(x,QP_1)RO_model_fxn(x,QP_1,Am,Lp,k,Q_0,pi_0),[0 1],[Q_0,dP_0]); 
@@ -18,12 +19,15 @@ dP_0 = 40.6+360*0.433-16.4; % Transmembrane Osmotic Inlet Pressure (psi)
 % Given Parameters
 Q_1 = QP_1(end,1); % Final Feed Flow Rate Value from Stage 1 (gpm)
 dP_1 = QP_1(end,2); % Final Transmembrane Osmotic Inlet Pressure Value from Stage 1 (psi)
-Am2 = Am/2; % Membrane Area (ft^2)
-Lp2 = Lp; % Membrane Hydraulic Permeability (gpm/psi)
+Am_2 = Am/2; % Membrane Area (ft^2)
+Lp_2 = Lp; % Membrane Hydraulic Permeability (gpm/psi)
 k2 = k*4; % Constant for Stage 2(psi/gpm^2)
 
 % Initialize  Diff. EQ for Stage 2
-[x2,QP_2]=ode45(@(x,QP_1)RO_model_fxn(x,QP_1,Am2,Lp2,k2,Q_0,pi_0),[1 2],[Q_1,dP_1]);
+[x2,QP_2]=ode45(@(x,QP_1)RO_model_fxn(x,QP_1,Am_2,Lp_2,k2,Q_0,pi_0),[1 2],[Q_1,dP_1]);
+
+% Solve for Change in Pressure
+Y = 1 - (QP_2(end,1)/Q_0);
 
 %% Graph Results 
 set(gca,'DefaultAxesFontSize',20)
