0e2504b7789985ee2a53d351bc63a0778a234ac3
goldpandasnow <159591191+goldpandasnow@users.noreply.github.com>
2024-03-14 21:22:14 -0700

    Update RO_model_fxn.m

--- Reverse Osmosis/RO_model_fxn.m
+++ Reverse Osmosis/RO_model_fxn.m
@@ -1,8 +1,7 @@
-%% Part A, 1.)
+%% Part A, 1.) 
 function dQP = RO_model_fxn(x,QP_1,Am,Lp,k,Q_0,pi_0)
 dQP = zeros(2,1);
-%% QP(1): Q
+%% QP(1): Q - Transmembrane Water Flow Rate 
 dQP(1) = -Am*Lp*(QP_1(2)-(Q_0*pi_0/QP_1(1)));
-%% QP(2): P
-dQP(2) = -k*QP_1(1)*QP_1(1);
-
+%% QP(2): P - System Pressure
+dQP(2) = -k*(QP_1(1)^2); %*QP_1(1);
