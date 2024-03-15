e1876ab604669544f15103c9052a02427cf0335d
goldpandasnow <159591191+goldpandasnow@users.noreply.github.com>
2024-03-14 19:37:39 -0700

    Update RO_model_fxn.m

--- Reverse Osmosis/RO_model_fxn.m
+++ Reverse Osmosis/RO_model_fxn.m
@@ -1,8 +1,8 @@
 %% Part A, 1.)
 function dQP = RO_model_fxn(x,QP_1,Am,Lp,k,Q_0,pi_0)
-%% QP(1): Q
-%% QP(2): P
 dQP = zeros(2,1);
+%% QP(1): Q
 dQP(1) = -Am*Lp*(QP_1(2)-(Q_0*pi_0/QP_1(1)));
+%% QP(2): P
 dQP(2) = -k*QP_1(1)*QP_1(1);
 
